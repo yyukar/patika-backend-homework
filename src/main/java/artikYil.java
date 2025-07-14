5b322d3d21f48eb995e5622b87d556bec3e4a95b
yusuf yukar <fusuyrakuy@gmail.com>
2025-07-14 18:12:58 +0300

    Artık Yıl Hesaplama eklendi.

---
+++ src/main/java/artikYil.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class artikYil {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Yıl Giriniz :");
+        int date = scanner.nextInt();
+        if (date<1) {
+            System.out.println("Hatalı Veri Girdiniz !");
+            return;
+        }
+        if((date % 4 ==0) && date % 100 != 0 || (date % 400 == 0)) {
+            System.out.println(date + " bir artık yıldır !");
+        }else {
+            System.out.println(date + "bir artık yıl değildir!");
+        }
+    }
+}
