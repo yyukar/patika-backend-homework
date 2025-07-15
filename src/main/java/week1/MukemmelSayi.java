f5bff5685db52726cafd8769b743c94aacd1c8da
yusuf yukar <fusuyrakuy@gmail.com>
2025-07-15 19:23:28 +0300

    Mükemmel Sayı Bulan Program eklendi.

---
+++ src/main/java/week1/MukemmelSayi.java
@@ -0,0 +1,25 @@
+package week1;
+
+import java.util.Scanner;
+
+public class MukemmelSayi {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("bir sayı giriniz: ");
+        int input = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= input ; i++) {
+            if (input % i == 0 ){
+                sum += i;
+            }
+        }
+
+        if(sum == input*2) {
+            System.out.println(input + " Mükemmel sayıdır");
+        } else {
+            System.out.println(input + " Mükemmel sayı değildir.");
+        }
+    }
+}
