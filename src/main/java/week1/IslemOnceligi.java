93c2145e24a0809a35afa794d38839efdb298ff1
yusuf yukar <fusuyrakuy@gmail.com>
2025-07-17 10:24:49 +0300

    Pratik - Aritmetik İşlemler ve İşlem Önceliği geri bildirimlere göre revize yapıldı.

--- src/main/java/week1/IslemOnceligi.java
+++ src/main/java/week1/IslemOnceligi.java
@@ -1,3 +1,10 @@
+/*
+ Pratik - Aritmetik İşlemler ve İşlem Önceliği
+ Soru: Kullanıcıdan 3 tane sayı (a, b, c) alıp bu sayıları işlem sırasına göre işleyen bir program yazın.
+ İşlem sırası şöyle olsun: a + b * c - b
+ İşlem tamamladıktan sonra sonucu ekrana yazdırın.
+*/
+
 package week1;
 
 import java.util.Scanner;
@@ -6,22 +13,27 @@ public class IslemOnceligi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("birinci sayiyi giriniz:");
+        // Kullanıcıdan sayıları al
+        System.out.println("Birinci sayıyı giriniz: ");
         int a = scanner.nextInt();
 
-        System.out.println("ikinci sayiyi giriniz:");
+        System.out.println("İkinci sayıyı giriniz: ");
         int b = scanner.nextInt();
 
-        System.out.println("ucuncu sayiyi giriniz: ");
+        System.out.println("Üçüncü sayıyı giriniz: ");
         int c = scanner.nextInt();
 
+        // Girdi kontrolü
         if(a<=0 || b<=0 || c<=0 ) {
             System.out.println("Lütfen pozitif sayılar giriniz!");
+            scanner.close();
             return;
         }
 
+        // İstenen işlem yapılır ve sonuç yazdırılır
         int sonuc = a+(b*c)-b;
+        System.out.println("Sonuç: " + sonuc);
 
-        System.out.println("sonuc : " + sonuc);
+        scanner.close();
     }
 }
