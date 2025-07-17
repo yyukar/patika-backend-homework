13ae2a689c9a2ed28a34101237f481eed3e9753e
yusuf yukar <fusuyrakuy@gmail.com>
2025-07-17 13:45:19 +0300

    Uçak Bileti Fiyatı Hesaplama Programı geri bildirimlere göre revize yapıldı.

--- src/main/java/week1/UcakBiletFiyati.java
+++ src/main/java/week1/UcakBiletFiyati.java
@@ -1,3 +1,17 @@
+/*
+Java ile mesafeye ve şartlara göre uçak bileti fiyatı hesaplayan programı yapın.
+Kullanıcıdan Mesafe (KM), yaşı ve yolculuk tipi (Tek Yön, Gidiş-Dönüş) bilgilerini alın.
+Mesafe başına ücret 0,10 TL / km olarak alın.
+İlk olarak uçuşun toplam fiyatını hesaplayın ve sonrasında ki koşullara göre müşteriye aşağıdaki indirimleri uygulayın ;
+
+Kullanıcıdan alınan değerler geçerli (mesafe ve yaş değerleri pozitif sayı, yolculuk tipi ise 1 veya 2) olmalıdır.
+Aksi takdirde kullanıcıya "Hatalı Veri Girdiniz !" şeklinde bir uyarı verilmelidir.
+Kişi 12 yaşından küçükse bilet fiyatı üzerinden %50 indirim uygulanır.
+Kişi 12-24 yaşları arasında ise bilet fiyatı üzerinden %10 indirim uygulanır.
+Kişi 65 yaşından büyük ise bilet fiyatı üzerinden %30 indirim uygulanır.
+Kişi "Yolculuk Tipini" gidiş dönüş seçmiş ise bilet fiyatı üzerinden %20 indirim uygulanır.
+*/
+
 package week1;
 
 import java.util.Scanner;
@@ -6,6 +20,7 @@ public class UcakBiletFiyati {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Kullanıcıdan girişler alınır
         System.out.println("Mesafeyi km türünden giriniz :");
         int distance = scanner.nextInt();
         double sum = distance * 0.10;
@@ -16,13 +31,16 @@ public class UcakBiletFiyati {
         System.out.println("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): 2 ");
         int travelType = scanner.nextInt();
 
+        // Geçersiz veri kontrolü
         if (distance <= 0 || age <= 0 || (travelType != 1 && travelType != 2)) {
             System.out.println("Hatalı Veri Girdiniz !");
+            scanner.close();
             return;
         }
 
+        // Yolculuk tipleri ve toplam hesabı
         switch (travelType) {
-
+            // Yolculuk tipi 1: Tek yön
             case 1 :
                 if(age < 12 ) {
                     double ageDiscount = sum * 0.5;
@@ -41,6 +59,7 @@ public class UcakBiletFiyati {
                 }
                 break;
 
+            // Yolculuk tipi 2: Gidiş-Dönüş
             case 2 :
                 if (age <12) {
                     double ageDiscount = sum * 0.5;
@@ -70,5 +89,7 @@ public class UcakBiletFiyati {
             default:
                 System.out.println("Hatalı Veri Girdiniz !");
         }
+
+        scanner.close();
     }
 }
