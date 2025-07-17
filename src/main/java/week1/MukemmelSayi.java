3466a5d69f79cca981b8a5e3139463851cfaa2f2
yusuf yukar <fusuyrakuy@gmail.com>
2025-07-17 17:07:33 +0300

    Mükemmel Sayı Bulan Program geri bildirimlere göre revize edildi.

--- src/main/java/week1/MukemmelSayi.java
+++ src/main/java/week1/MukemmelSayi.java
@@ -1,25 +1,46 @@
+/*
+Klavyeden girilen bir sayının mükemmel sayı olup/olmadığını bulan ve sayı mükemmel sayı ise ekrana
+“mükemmel sayıdır.” değilse “mükemmel sayı değildir.” ifadelerini ekrana yazan programı Java dilinde yazınız.
+Mükemmel Sayı Nedir?
+
+Bir sayının kendisi hariç pozitif tam sayı çarpanları (kalansız bölen sayıların) toplamı kendisine eşit olan
+ sayıya mükemmel sayı denir.
+ */
+
 package week1;
 
 import java.util.Scanner;
 
 public class MukemmelSayi {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("bir sayı giriniz: ");
-        int input = scanner.nextInt();
-        int sum = 0;
 
+        // Kullanıcıdan pozitif bir tam sayı alınır
+        System.out.println("Bir sayı giriniz: ");
+        int input = scanner.nextInt();
+
+        // Geçersiz veri kontrolü
+        if (input < 1) {
+            System.out.println("Lütfen 1 veya daha büyük bir sayı giriniz.");
+            scanner.close();
+            return;
+        }
+
+        // Bölenlerin toplamı hesaplanır
+        int sum = 0;
         for (int i = 1; i < input ; i++) {
             if (input % i == 0 ){
                 sum += i;
             }
         }
 
+        // Mükemmel sayı kontrolü
         if(sum == input) {
             System.out.println(input + " Mükemmel sayıdır");
         } else {
             System.out.println(input + " Mükemmel sayı değildir.");
         }
+
+        scanner.close();
     }
 }
