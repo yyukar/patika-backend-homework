20ebd4eb99089e43273c35cb00b8b040894569a8
yusuf yukar <fusuyrakuy@gmail.com>
2025-07-22 00:05:17 +0300

    Üs Hesabı Yapan Program eklendi.

--- src/main/java/week2/mathPow/MathPow.java
+++ src/main/java/week2/mathPow/MathPow.java
@@ -8,47 +8,28 @@ public class MathPow {
 
         // Kullanıcıdan taban sayısı alınır
         System.out.print("Üssünü almak istediğiniz sayıyı giriniz: ");
-        double number = scanner.nextDouble();
+        double base = scanner.nextDouble();
 
         // Kullanıcıdan üs değeri alınır
         System.out.print("Üs değerini giriniz: ");
         double exponent = scanner.nextDouble();
 
-        // Math.pow alternatif | START
-        if (exponent % 1 != 0) {
-            System.out.println("Sonuç: " + Math.pow(number,exponent));
-            scanner.close();
-            return;
-        }
-        // Math.pow alternatif | END
-
-        // Tam sayı üs değeri için özel metot kullanılır ve yazdırılır
-        System.out.println("Sonuç: " + calculateMathPow(number, exponent));
-        scanner.close();
+        // power metodu ile üs hesaplanır ve yazdırılır.
+        System.out.println(power(base,exponent));
     }
 
     /**
      * Verilen sayının üssünü hesaplayan metot
      *
-     * @param number   Taban sayı
+     * @param base   Taban sayı
      * @param exponent Üs değeri
      * @return Üs alma işleminin sonucu
      */
-    public static double calculateMathPow(double number, double exponent){
-        double sum= 1.0;
-        if (exponent > 0){
-            for (int i = 0; i < exponent; i++) {
-                sum *= number;
-            }
-            return sum;
-        } else if (exponent < 0) {
-            exponent = -1 * exponent;
-            for (int i = 0; i < exponent; i++) {
-                sum *= number;
-            }
-            return 1.0 /sum;
-        } else {
-            return sum;
+    private static double power(double base, double exponent) {
+        if (exponent == 0 ) return 1;
+        if (exponent < 0) {
+            return (1 / base) * power(base,exponent + 1);
         }
+        return base * power(base,exponent - 1);
     }
 }
